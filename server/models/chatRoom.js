870798d8221ce7a4e4b3a28ae0f7987514fdfac2
Fonji Fonkeng Rocard <rocardpp@gmail.com>
2021-03-02 00:07:34 +0100

    * created chatRoom model  * model functions to getChatRoomsByUserId, getChatRoomByRoomId, initiateChat

---
+++ server/models/chatRoom.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+const chatRoomSchema = new mongoose.Schema(
+  {
+    chatInitiator: { type: String, required: true },
+    userIds: { type: Array, required: true },
+  },
+  {
+    timestamps: true,
+    collection: "chatRoom",
+  }
+);
+
+chatRoomSchema.statics.getChatRoomsByUserId = async function (userId) {
+  try {
+    const rooms = await this.find({ userIds: { $all: [userId] } });
+    return rooms;
+  } catch (error) {
+    throw error;
+  }
+};
+
+chatRoomSchema.statics.getChatRoomByRoomId = async function (roomId) {
+  try {
+    const room = await this.findOne({ _id: roomId });
+    return room;
+  } catch (error) {
+    throw error;
+  }
+};
+
+chatRoomSchema.statics.initiateChat = async function (userIds, chatInitiator) {
+  try {
+    const availableRoom = await this.findOne({
+      userIds: {
+        $size: userIds.length,
+        $all: [...userIds],
+      },
+      chatInitiator
+    });
+    if (availableRoom) {
+      return {
+        isNew: false,
+        chatRoomId: availableRoom._doc._id,
+      };
+    }
+
+    const newRoom = await this.create({ userIds, chatInitiator });
+    return {
+      isNew: true,
+      chatRoomId: newRoom._doc._id,
+    };
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default mongoose.model("ChatRoom", chatRoomSchema);
